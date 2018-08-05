29339bd55f3cacd9f84483bb077babcdb6e0c227
QingyiHuang <297947067@qq.com>
2018-08-05 22:43:34 +0800

    emmm

---
+++ 封装兼容性/任意属性animate.js
@@ -0,0 +1,62 @@
+  //点击按钮,改变宽度到达一个目标值,高度到达一个目标值
+
+  //获取任意一个元素的任意一个属性的当前的值---当前属性的位置值
+  function getStyle(element, attr) {
+    return window.getComputedStyle ? window.getComputedStyle(element, null)[attr] : element.currentStyle[attr] || 0;
+  }
+
+//index 和透明度要单独操作 所以专门多列两个分支
+//回调函数，当前面事件执行后，就执行回调函数
+  function animate(element, json, fn) {
+    clearInterval(element.timeId);//清理定时器
+    //定时器,返回的是定时器的id
+    element.timeId = setInterval(function () {
+      var flag = true;//默认,假设,全部到达目标
+      //遍历json对象中的每个属性还有属性对应的目标值
+      for (var attr in json) {
+        //判断这个属性attr中是不是opacity
+        if (attr == "opacity") {
+          //获取元素的当前的透明度,当前的透明度放大100倍 为了方便计算
+          var current = getStyle(element, attr) * 100;
+          //目标的透明度放大100倍
+          var target = json[attr] * 100;
+          var step = (target - current) / 10;
+          step = step > 0 ? Math.ceil(step) : Math.floor(step);
+          current += step;//移动后的值
+          element.style[attr] = current / 100;
+        } else if (attr == "zIndex") { //判断这个属性attr中是不是zIndex
+          //层级改变就是直接改变这个属性的值
+          element.style[attr] = json[attr];
+        } else {
+          //普通的属性
+          //获取元素这个属性的当前的值
+          var current = parseInt(getStyle(element, attr));
+          //当前的属性对应的目标值
+          var target = json[attr];
+          //移动的步数
+          var step = (target - current) / 10;
+          step = step > 0 ? Math.ceil(step) : Math.floor(step);
+          current += step;//移动后的值
+          element.style[attr] = current + "px";
+        }
+        //是否到达目标
+        if (current != target) {
+          flag = false;
+        }
+      }
+      if (flag) {
+        //清理定时器
+        clearInterval(element.timeId);
+        //所有的属性到达目标才能使用这个函数,前提是用户传入了这个函数
+        if (fn) {
+          fn();
+        }
+      }
+      //测试代码
+      console.log("目标:" + target + ",当前:" + current + ",每次的移动步数:" + step);
+    }, 20);
+  }
+
+
+  //zIndex:1000
+  //透明度: 数字类型----小数---放大100倍
